70f75d0d11a9cecbed4f72857d8faa789982cf64
PremKumar Sahebraj Mistry <120615589+premkumarmistry@users.noreply.github.com>
2024-05-02 21:05:26 +0530

    Create mainactivity2.java

---
+++ Five/mainactivity2.java
@@ -0,0 +1,26 @@
+package com.sneha.practical5;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.widget.TextView;
+
+import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+
+public class MainActivity2 extends AppCompatActivity {
+    TextView textview;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        EdgeToEdge.enable(this);
+        setContentView(R.layout.activity_main2);
+        Intent intent=getIntent();
+        String message= intent.getStringExtra("Addition");
+        textview=findViewById(R.id.tv1);
+        textview.setText(message);
+    }
+}
