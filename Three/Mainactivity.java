f3c900210f6f5a819a636408f7a96471795c8f9f
PremKumar Sahebraj Mistry <120615589+premkumarmistry@users.noreply.github.com>
2024-05-02 21:00:16 +0530

    Create Mainactivity.java

---
+++ Three/Mainactivity.java
@@ -0,0 +1,25 @@
+package com.sneha.practical3;
+
+import android.os.Bundle;
+import android.widget.EditText;
+import android.widget.Toast;
+
+import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        EdgeToEdge.enable(this);
+        setContentView(R.layout.activity_main);
+
+            EditText e = findViewById(R.id.edit_text);
+            Toast.makeText(getApplicationContext(),e.getText().toString(),Toast.LENGTH_SHORT).show();
+
+    }
+}
